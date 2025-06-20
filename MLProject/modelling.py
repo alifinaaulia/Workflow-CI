bf542172e40aacf2973eacc3ae7fdab5f9fb0b57
alifina aulia <alifinaaulia@alifinas-MacBook-Air.local>
2025-06-20 15:06:42 +0700

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -47,7 +47,7 @@ def run_hybrid_modeling(n_components):
     if mlflow.active_run():
         mlflow.end_run()
     mlflow.set_tracking_uri("file://" + os.path.abspath("mlruns"))
-    with mlflow.start_run(run_name="Hybrid Recommender") as run:
+    with mlflow.start_run(run_name="Hybrid Recommender", nested=True) as run:
         # Load from GitHub Actions env or fallback to local
         csv_url = os.getenv("CSV_URL", "MLProject/online_retail_preprocessing.csv")
         df = pd.read_csv(csv_url)
